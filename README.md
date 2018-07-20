a83b0eeddc9fdc58f0c7e63f34be8158c2d74e57
cvaize <cvaize@gmail.com>
2018-07-20 22:59:21 +0500

    Working

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 Это стартовый пакет Laravel + Laratrust + Translation + Collective. Пакет тестировался на Laravel 5.6
 
 # Внимание!!! Используйте этот пакет только на проект, который не содержит вашего кода. Команда на установку заменяет стандартные файлы
+
+Вставьте это в файл composer.json
+
+`"scripts": {
+     "post-update-cmd": [
+         ... other stuff ...
+         "php artisan vendor:publish --provider=\"Vsch\\TranslationManager\\ManagerServiceProvider\" --tag=public --force",
+         ... other stuff ...
+     ]
+ },`
--- src/LaratrustAdminServiceProvider.php
+++ src/LaratrustAdminServiceProvider.php
@@ -16,7 +16,29 @@ class StartedPackAdminLaravelServiceProvider extends ServiceProvider
 
     public function register()
     {
-
+        Artisan::command('StartedPackAdminLaravel:init', function () {
+            $this->info("StartedPackAdminLaravel Initialization");
+            $cmd = 'php artisan StartedPackAdminLaravel:install';
+//            $cmd .= ' && php artisan ladmin:routes';
+//            $cmd .= ' && php artisan ladmin:install';
+//            $cmd .= ' && php artisan ladmin:publish:laratrust';
+//            $cmd .= ' && php artisan ladmin:files:delete';
+//            $cmd .= ' && php artisan ladmin:publish:config';
+//            $cmd .= ' && php artisan ladmin:publish:front';
+//            $cmd .= ' && php artisan ladmin:publish:admin';
+//            $cmd .= ' && php artisan ladmin:migration';
+//            $cmd .= ' && php artisan ladmin:cache';
+//            $cmd .= ' && composer dump-autoload';
+            system($cmd);
+        });
+        Artisan::command('StartedPackAdminLaravel:install', function () {
+            $this->info("StartedPackAdminLaravel install");
+            $cmd = 'composer update';
+            $cmd .= '&& php artisan make:auth';
+            $cmd .= '&& composer require "santigarcor/laratrust:^5.0" "laravelcollective/html:^5.6.10" "fzaninotto/faker:^1.8.0" ';
+            $cmd .= '"mcamara/laravel-localization:^1.3" "vsch/laravel-translation-manager:~2.6" "barryvdh/laravel-debugbar:^3.1"';
+            system($cmd);
+        });
     }
 
 
