d8d2c46f65bee3a5e68734a698514467a7a891d6
cvaize <cvaize@gmail.com>
2018-07-20 20:09:38 +0500

    Init

---
+++ src/LaratrustAdminServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace StartedPackAdminLaravel;
+
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Artisan;
+
+class StartedPackAdminLaravelServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+
+
+}
