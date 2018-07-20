43671680695504a802a3531f3f6ac0a45d139467
cvaize <cvaize@gmail.com>
2018-07-21 00:39:47 +0500

    Working

--- src/LaratrustAdminServiceProvider.php
+++ src/LaratrustAdminServiceProvider.php
@@ -70,7 +70,6 @@ class StartedPackAdminLaravelServiceProvider extends ServiceProvider
                 base_path('resources/assets/js/app.js'),
                 base_path('resources/assets/sass/app.scss'),
                 base_path('routes/web.php'),
-                base_path('app/Http/Kernel.php'),
                 base_path('database/seeds/DatabaseSeeder.php'),
                 base_path('webpack.mix.js'),
             ]);
@@ -78,6 +77,7 @@ class StartedPackAdminLaravelServiceProvider extends ServiceProvider
         // base_path('bootstrap/app.php'),
         // base_path('app/Providers')
         // base_path('app/config')
+        // base_path('app/Http/Kernel.php'),
         Artisan::command('StartedPackAdminLaravel:publish', function () {
             $this->info("StartedPackAdminLaravel publish");
             $date = date('Y_m_d_His');
